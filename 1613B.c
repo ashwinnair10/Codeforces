658db2515e9d8ca69d9f23bca1f51648b8b9282f
ashwinnair10 <ashwinnair1894@gmail.com>
2024-04-06 10:23:59 +0530

    1613B

--- 1613B.c
+++ 1613B.c
@@ -1,24 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
-int cmpfunc(const void* a,const void* b){
-   return (*(int*)a-*(int*)b);
+void init(int hash[]){
+    for(int i=0;i<1000000;i++)
+    hash[i]=0;
+}
+int cmp(const void* a,const void* b){
+   return(*(int*)a-*(int*)b);
 }
 int main(){
+    int hash[1000000]={0};
     int t;
     scanf("%d",&t);
-    while(t){
+    while(t--){
         int n,c=0;
         scanf("%d",&n);
         int a[n];
-        for(int i=0;i<n;i++)
-        for(int i=0;i<n;i++)
-        scanf("%d",&a[i]);
-        qsort(a,n,sizeof(int),cmpfunc);
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                
-            }
+            scanf("%d",&a[i]);
+            hash[a[i]]=1;
         }
-        t--;
+        qsort(a,n,sizeof(int),cmp);
+        for(int i=1;i<n;i++){
+            printf("%d %d\n",a[i],a[0]);
+            c++;
+            if(c==n/2)
+            break;
+        }
+        for(int i=0;i<n;i++)
+        hash[a[i]]=0;
     }
 }
